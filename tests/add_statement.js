a3dc33491a7b86570d18a50315b89e006a14c00f
maxlath <k@maxlath.eu>
2021-04-03 10:37:25 +0200

    add support for qualifiers

--- tests/add_statement.js
+++ tests/add_statement.js
@@ -1,35 +1,43 @@
 require('should')
 const convert = require('../lib/convert')
 
-describe('convert', () => {
-  it('should convert an entity statement', () => {
+describe('add statement', () => {
+  it('should add an entity statement', () => {
     convert('Q4115189	P31	Q1').edits[0].claims.P31[0]
     .should.equal('Q1')
   })
 
-  it('should convert a string statement', () => {
+  it('should add a string statement', () => {
     convert('Q4115189	P373	"Antoni Ignacy Mietelski"').edits[0].claims.P373[0]
     .should.deepEqual('Antoni Ignacy Mietelski')
   })
 
-  it('should convert a monolingual text statement', () => {
+  it('should add a monolingual text statement', () => {
     convert('Q1214098	P1476	pl:"Krzyżacy"').edits[0].claims.P1476[0]
     .should.deepEqual({ text: 'Krzyżacy', language: 'pl' })
   })
 
-  it('should convert a time statement', () => {
+  it('should add a time statement', () => {
     convert('Q41576483	P569	+1839-00-00T00:00:00Z/9').edits[0].claims.P569[0]
     .should.deepEqual({ time: '+1839-00-00T00:00:00Z', precision: 9 })
   })
 
   // How to set the precision?
-  xit('should convert a location coordinates statement', () => {
+  xit('should add a location coordinates statement', () => {
     convert('Q3669835	P625	@43.26193/10.92708').edits[0].claims.P625[0]
     .should.deepEqual({ latitude: 43.26193, longitude: 10.92708, precision: '?' })
   })
 
-  it('should convert a quantity statement', () => {
+  it('should add a quantity statement', () => {
     convert('Q41576483	P1106	123~0.5U11573').edits[0].claims.P1106[0]
     .should.deepEqual({ amount: 123, unit: 'Q11573', lowerBound: 122.5, upperBound: 123.5 })
+  })
+
+  it('should add a statement with a qualifier', () => {
+    convert('Q41577083	P570	+1600-00-00T00:00:00Z/7	P1319	+1586-00-00T00:00:00Z/9').edits[0].claims.P570[0]
+    .should.deepEqual({
+      value: { time: '+1600-00-00T00:00:00Z', precision: 7 },
+      qualifiers: { P1319: [ { time: '+1586-00-00T00:00:00Z', precision: 9 } ] }
+    })
   })
 })
